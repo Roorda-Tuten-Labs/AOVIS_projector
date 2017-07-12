9a006946c741849c4a3d36fa92d60494c3fb6810
bps10 <brian.patrick.schmidt@gmail.com>
2017-07-12 14:50:53 -0700

    correct handling of case without logitech keyboard

--- +white/+fil/find_keyboard_index.m
+++ +white/+fil/find_keyboard_index.m
@@ -5,8 +5,16 @@ function keyboard_index = find_keyboard_index
     % use Logitech external keyboard if it is plugged in. First find ID
     [keyboard_index, name, infos] = GetKeyboardIndices('USB Receiver');
     % check that it is correct
-    if ~strcmpi(infos{1}.manufacturer, 'logitech') || ...
+    
+    if isempty(infos)
+        keyboard_index = -1;
+        warning(['Logitech USB keybard not found. ' ...
+            'Using default -1 instead. This cause the program to' ...
+            ' check all plugged in keyboards.']);
+        
+    elseif ~strcmpi(infos{1}.manufacturer, 'logitech') || ...
             ~strcmpi(name, 'USB Receiver')
+        % shouldn't ever get to here, but just double check
         [keyboard_index, name, ~] = GetKeyboardIndices();
         warning(['Logitech USB keybard not found. Using ' name ' instead.']);
     end
